94fec750bc6ee195e412b47ca28d6804ed57ff00
Afel <prteza@gmail.com>
2022-11-12 04:06:40 -0800

    Basic parts of uci working
    Works with fixed time nothing else

--- uci/uci.go
+++ uci/uci.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"fmt"
+	"strconv"
 	"strings"
 	"gnomethulu/mcts"
 	"github.com/dylhunn/dragontoothmg"
@@ -14,9 +15,15 @@ func Init(treeFunc func(*mcts.MonteCarloNode, *mcts.MonteCarloNode, dragontoothm
 
 	searcher := mcts.NewSearch(treeFunc, evalFunc)
 
+	f, _ := os.Create("./input_log.txt")
+ 	w := bufio.NewWriter(f)
+
 	for true {
 		line, _ := reader.ReadString('\n')
+		w.WriteString(line)
+		w.Flush()
 		line = strings.TrimSpace(line)
+		//TODO: we should make all white space a single space character to simplify edge cases
 
 		command := ""
 		arguments := ""
@@ -44,15 +51,48 @@ func Init(treeFunc func(*mcts.MonteCarloNode, *mcts.MonteCarloNode, dragontoothm
 				searcher = mcts.NewSearch(treeFunc, evalFunc)
 				searcher = searcher
 			case "position":
-
+				fenString := strings.TrimSpace(GetStringBefore(arguments, "moves"))
+				moveStrings := strings.Split(strings.TrimSpace(GetStringAfter(arguments, "moves")), " ")
+				if fenString != "startpos" {
+					searcher.SetPosition(fenString)
+				} else {
+					searcher.SetPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+				}
+				for _, move := range moveStrings {
+					searcher.ApplyStr(move)
+				}
 			case "go":
+				timethinker := GetStringAfter(arguments,"movetime")
+				if timethinker != "" {
+					i, _ := strconv.Atoi(strings.TrimSpace(timethinker))
+					searcher.RunTime(float64(i)/1000)
+					move := searcher.GetBestMove()
+					fmt.Println("bestmove", move.String())
+				}
+
+				//TODO: use channels to let ai respond while thinking
+				//TODO: constantly transmit data
+			case "stop":
+				move := searcher.GetBestMove()
+				fmt.Println("bestmove", move.String())
+				fmt.Println("info string", "stop", arguments)
 		}
-		//fmt.Println(line)
+		
 	}
 }
 
-/* c2s
-uci
-debug
+func GetStringBefore(str string, end string) string {
+	e := strings.Index(str, end)
+	if e == -1 {
+		return ""
+	}
+	return str[:e]
+}
 
-*/
+func GetStringAfter(str string, start string) string {
+	s := strings.Index(str, start)
+	if s == -1 {
+		return ""
+	}
+	return str[s+len(start):]
+}
