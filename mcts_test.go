78f4b76f977bc32ff7f551581731643c6fdc9c49
Afel <prteza@gmail.com>
2022-12-10 14:27:47 -0800

    new test
    Trying to fix mate search

--- mcts_test.go
+++ mcts_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestMCTS(t *testing.T){
 	searcher := search.NewSearch(policy.UCT, traditional.CustomV1)
-	searcher.SetPosition(dragontoothmg.ParseFen("rnbqkb1r/ppp2ppp/8/3pN1nQ/8/8/PPPP1PPP/RNB1KB1R w KQkq - 0 6"))
+	searcher.SetPosition(dragontoothmg.ParseFen("4rk2/3QRppp/2p5/p1q5/P1P5/6pP/1P3PP1/4R1K1 b - - 0 29"))
 
 	searcher.RunIterations(500000)
 	t.Log(searcher.Head.Visits)
